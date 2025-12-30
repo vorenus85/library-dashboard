c8d2059e045c035bac3ddda33e702afaa741ed8a
János Perge <janos.perge@gmail.com>
2025-12-30 17:36:26 +0100

    create sidebarMenuitem component

---
+++ resources/js/components/SidebarMenuitem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="sidebar-menuitem">
+        <RouterLink :to="path"
+            ><i :class="`pi pi-fw pi-${icon} sidebar-menuitem-icon`"></i> {{ title }}</RouterLink
+        >
+    </li>
+</template>
+<script setup>
+defineProps({
+    icon: { type: String, required: true },
+    path: { type: String, required: true },
+    title: { type: String, required: true },
+})
+</script>
--- resources/js/layout/AppSideBar.vue
+++ resources/js/layout/AppSideBar.vue
@@ -1,32 +1,40 @@
 <template>
     <div class="layout-sidebar">
         <ul class="sidebar-menu">
-            <li class="sidebar-menuitem">
-                <RouterLink to="/"
-                    ><i class="pi pi-fw pi-home sidebar-menuitem-icon"></i> Dashboard</RouterLink
-                >
-            </li>
-            <li class="sidebar-menuitem">
-                <RouterLink to="/books"
-                    ><i class="pi pi-fw pi-book sidebar-menuitem-icon"></i> Books</RouterLink
-                >
-            </li>
-            <li class="sidebar-menuitem">
-                <RouterLink to="/whislist"
-                    ><i class="pi pi-fw pi-bookmark sidebar-menuitem-icon"></i> Whislist</RouterLink
-                >
-            </li>
-            <li class="sidebar-menuitem">
-                <RouterLink to="/authors"
-                    ><i class="pi pi-fw pi-user sidebar-menuitem-icon"></i> Authors</RouterLink
-                >
-            </li>
-            <li class="sidebar-menuitem">
-                <RouterLink to="/genres"
-                    ><i class="pi pi-fw pi-tags sidebar-menuitem-icon"></i> Genre</RouterLink
-                >
-            </li>
+            <template v-for="menu in menus">
+                <SidebarMenuitem :icon="menu.icon" :path="menu.path" :title="menu.title" />
+            </template>
         </ul>
     </div>
 </template>
-<script setup></script>
+<script setup>
+import SidebarMenuitem from '../components/SidebarMenuitem.vue'
+
+const menus = [
+    {
+        icon: 'home',
+        path: '/',
+        title: 'Dashboard',
+    },
+    {
+        icon: 'book',
+        path: '/books',
+        title: 'Books',
+    },
+    {
+        icon: 'bookmark',
+        path: '/whislist',
+        title: 'Whislist',
+    },
+    {
+        icon: 'user',
+        path: '/authors',
+        title: 'Authors',
+    },
+    {
+        icon: 'tags',
+        path: '/genres',
+        title: 'Genres',
+    },
+]
+</script>

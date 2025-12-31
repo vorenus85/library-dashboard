05295afa720c9b0ecef2b8f2548cebf5fe84250d
János Perge <janos.perge@gmail.com>
2025-12-31 20:43:52 +0100

    add topbar icons

--- resources/js/layout/AppTopBar.vue
+++ resources/js/layout/AppTopBar.vue
@@ -1,13 +1,19 @@
 <template>
     <header class="bg-white shadow layout-topbar">
         <div class="layout-topbar-logo-container">
-            <button type="button" class="layout-menu-button layout-topbar-action">
+            <button
+                @click="toggleSiderbar"
+                type="button"
+                class="layout-menu-button layout-topbar-action"
+            >
                 <UiIcon icon="bars"></UiIcon>
             </button>
-            <div class="app-name">Library</div>
+            <div class="app-name">
+                <router-link to="/">Library</router-link>
+            </div>
         </div>
         <div class="layout-topbar-actions">
-            <button type="button" class="layout-topbar-action">
+            <button @click="toggleDarkMode" type="button" class="layout-topbar-action">
                 <UiIcon icon="sun"></UiIcon>
             </button>
         </div>
@@ -15,4 +21,12 @@
 </template>
 <script setup>
 import UiIcon from '../components/UiIcon.vue'
+
+const toggleSiderbar = () => {
+    alert('Todo toggle sidebar logic')
+}
+
+const toggleDarkMode = () => {
+    alert('Todo toggle dark mode logic')
+}
 </script>

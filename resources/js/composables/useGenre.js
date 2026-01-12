e9ab286008b5fa4a5ccae9ddcdb9c0e9ffdd65b8
János Perge <janos.perge@gmail.com>
2026-01-12 23:12:09 +0100

    refactor async await, use try catch blocks

--- resources/js/composables/useGenre.js
+++ resources/js/composables/useGenre.js
@@ -17,72 +17,68 @@ export function useGenre() {
 
     const getGenresMinimal = async () => {
         loading.value = true
-        return await axios
-            .get('/genres/?minimal=1')
-            .catch(error => {
-                console.error(error)
-            })
-            .then(response => {
-                genres.value = response.data
-            })
-            .finally(() => {
-                loading.value = false
-            })
+
+        try {
+            const { data } = await axios.get('/genres/?minimal=1')
+            genres.value = data
+            loading.value = false
+        } catch (e) {
+            console.log(e)
+            loading.value = false
+        }
     }
 
     const getGenres = async () => {
         loading.value = true
-        return await axios
-            .get('/genres')
-            .catch(error => {
-                console.error(error)
-            })
-            .then(response => {
-                genres.value = response.data
-            })
-            .finally(() => {
-                loading.value = false
-            })
+
+        try {
+            const { data } = await axios.get('/genres')
+            genres.value = data
+            loading.value = false
+        } catch (e) {
+            console.log(e)
+            loading.value = false
+        }
     }
 
     const getGenre = async () => {
         loading.value = true
-        return await axios
-            .get(`/genres/${genreId}`)
-            .then(response => {
-                initialValues.name = response.data.name
-                initialValues.description = response.data.description
-                formKey.value++ // to remount primevue/form to trigger form resolver/validation https://github.com/primefaces/primevue/issues/7792
-            })
-            .catch(e => {
-                console.error(e)
-            })
-            .finally(() => {
-                loading.value = false
-            })
+
+        try {
+            const { data } = await axios.get(`/genres/${genreId}`)
+            initialValues.name = data.name
+            initialValues.description = data.description
+            formKey.value++ // to remount primevue/form to trigger form resolver/validation https://github.com/primefaces/primevue/issues/7792
+            loading.value = false
+        } catch (e) {
+            console.log(e)
+            loading.value = false
+        }
     }
 
     const deleteGenre = async id => {
         loading.value = true
-        return await axios
-            .delete(`/genres/${id}`)
-            .catch(error => {
-                console.error(error)
+
+        try {
+            await axios.delete(`/genres/${id}`)
+
+            const idIndex = genres.value.findIndex(el => {
+                return el.id === id
             })
-            .then(() => {
-                const idIndex = genres.value.findIndex(el => {
-                    return el.id === id
-                })
-                genres.value.splice(idIndex, 1)
-                toast.add({
-                    severity: 'success',
-                    summary: 'Genre deleted successfully!',
-                    life: 3000,
-                })
+
+            genres.value.splice(idIndex, 1)
+
+            toast.add({
+                severity: 'success',
+                summary: 'Genre deleted successfully!',
+                life: 3000,
             })
-            .finally(() => {
-                loading.value = false
-            })
+
+            loading.value = false
+        } catch (e) {
+            console.log(e)
+            loading.value = false
+        }
     }
 
     return {

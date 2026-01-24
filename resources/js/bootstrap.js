9d03d23e84644156569d9cdfdf7e602c0a5267ef
Janos Perge <janos.perge@gmail.com>
2026-01-24 22:12:19 +0100

    remove hardcoded baseurl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8080'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.withCredentials = true

6991c5888c95d6152fb605690bffde07be658d7d
Janos Perge <janos.perge@gmail.com>
2026-01-27 10:43:17 +0100

    try fix phpstan error

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * @mixin Book
+ */
 class BookResource extends JsonResource
 {
     /**
